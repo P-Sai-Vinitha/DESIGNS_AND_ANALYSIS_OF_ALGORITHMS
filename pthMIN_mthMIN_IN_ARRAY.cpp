f192d75ec91139fb095b9f2bd8df8056b38c705a
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2023-02-02 20:38:32 +0530

    Add files via upload

---
+++ pthMIN_mthMIN_IN_ARRAY.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	int p,m;
+	scanf("%d %d",&p,&m);
+	printf( "minimum %d",arr[p-1]);
+	printf("\n");
+	printf("maximum %d",arr[n-m]);
+}

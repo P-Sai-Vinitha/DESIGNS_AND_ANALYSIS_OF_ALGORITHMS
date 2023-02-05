dd99ff3b7935e8a942bb02803e30d71843023ddd
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2023-02-05 09:50:18 +0530

    Add files via upload

---
+++ PERFECT_NUMBER.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,s;
+	printf("enter number:");
+	scanf("%d",&n);
+	s=0;
+	for(int i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			s+=i;
+		}
+	}
+	
+	if(s==n)
+	{
+		printf("it is a perfect number");
+	}
+	else
+	{
+		printf("it is not a perfect number");
+	}
+}
